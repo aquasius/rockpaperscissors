007aa96eb1a46a603fb9cf5200ed8ab40b2db7ec
Aaron Quasius <aquasius92@gmail.com>
2019-11-21 09:04:49 -0600

    forgot to save on my last commit. rounding up my round winner method. Added the ability to randomize the computers' name as well as have a game be computer vs computer. I've been super dialed in to the code that its breezing by and I'm finishing too much code instead of committing after each addition. Will slow down and make more committs.

--- AI.cs
+++ AI.cs
@@ -9,35 +9,34 @@ namespace RPSgame
     class AI : Player
     {
         // member vairables 
-        public string name;
+        
         private Random rand;
         public int gesturesCount;
-
+        public int nameCount;
+        private List<string> randNames;
 
         // constructor
+        public AI()
+        {
+            randNames = new List<string>() { "Wally", "Dorf", "Grub", "Loppy" };
+        }
 
 
 
         // member methods
-        private void randomizeGestures()
+        public override void chooseGesture()
         {
             int cpuGesture = rand.Next(gesturesCount);
             choice = gestures[cpuGesture];
             Console.WriteLine(choice);
-        }
 
-        
-
-        public override void chooseGesture()
-        {
-            
-       
         }
 
         public override void chooseName()
         {
-            Console.WriteLine("My name is Grub");
-            Console.ReadLine();
+            int cpuNames = rand.Next(nameCount);
+            name = randNames[cpuNames];
+            Console.WriteLine(name);
 
         }
 
--- Game.cs
+++ Game.cs
@@ -51,24 +51,30 @@ namespace RPSgame
 
         public void chooseNumberOfPlayers()
         {
-            Console.WriteLine("How many players are playing? 1 or 2?");
+            Console.WriteLine("How many players are playing? 0, 1 or 2?");
             numberOfPlayers = Convert.ToInt32(Console.ReadLine());
         }
 
         
         public void createPlayers(int numberOfPlayers)
         {
-            if(numberOfPlayers == 1)
-            {
+           if(numberOfPlayers == 0)
+           {
+                player1 = new AI();
+                player2 = new AI();
+           }
+            
+           else if(numberOfPlayers == 1)
+           {
                 player1 = new Human();
                 player2 = new AI();
-            }
+           }
         
-            else if(numberOfPlayers == 2)
-            {
+           else if (numberOfPlayers == 2)
+           {
                 player1 = new Human();
                 player2 = new Human();
-            }
+           }
         }
 
         public void determineGestureWin()
@@ -79,16 +85,55 @@ namespace RPSgame
                 Console.ReadLine();
             }
 
-            else if ((player1.choice == "rock") && (player2.choice == "scissors") || (player2.choice == "lizard")) 
+            else if ((player1.choice == "rock") && (player2.choice == "scissors") || (player2.choice == "lizard"))
             {
                 player1.score++;
                 Console.WriteLine("Rock beats " + player2.choice + "!");
                 Console.WriteLine(player1.name + " score is: " + player1.score + "\n" + player2.name + " score is: " + player2.score);
             }
 
+            else if ((player1.choice == "paper") && (player2.choice == "rock"))
+            {
+                player1.score++;
+                Console.WriteLine("Paper covers " + player2.choice + "!");
+            }
+
+            else if ((player1.choice == "scissors") && (player2.choice == "paper") || (player2.choice == "lizard"))
+            {
+                player1.score++;
+                Console.WriteLine("Scissors cuts " + player2.choice + "!");
+            }
+
+            else if ((player1.choice == "lizard") && (player2.choice == "spock") || (player2.choice == "paper"))
+            {
+                player1.score++;
+                Console.WriteLine("Lizard beats " + player2.choice + "!");
+            }
+
+            else if ((player1.choice == "spock") && (player2.choice == "scissors"))
+            {
+                player1.score++;
+                Console.WriteLine("Spock smashes " + player2.choice + "!");
+            }
+
+            else player2.score++;
         }
         
     
+        public void determineRoundWinner()
+        {
+            if(player1.score == 1)
+            {
+                Console.WriteLine(player1.name + " Wins the round!");
+                Console.WriteLine(player1.name + " score is: " + player1.score);
+                Console.WriteLine(player2.name + " score is: " + player2.score);
+            }
+            else if(player2.score == 1)
+            {
+                Console.WriteLine(player2.name + " Wins the round")
+            }
+            
+        }
         
     
     
